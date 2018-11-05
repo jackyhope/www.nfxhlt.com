1ca5630d0d1f3586eec47452dd8c96aaecc555f2
yanghao <389812249@qq.com>
2018-11-05 17:49:40 +0800

    @

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
 if (strpos($_SERVER['SERVER_ADDR'], '127.0.0.1') !== FALSE) {
-    require '../thinkphp/start.php';
-} else {
     require __DIR__ .'/../thinkphp/start.php';
+} else {
+    require '../thinkphp/start.php';
 }
