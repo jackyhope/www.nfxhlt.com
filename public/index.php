48187358268b7f8443dd66e8d2612b702dbac3f6
yanghao <389812249@qq.com>
2018-11-05 17:54:37 +0800

    @添加thinkphp

--- public/index.php
+++ public/index.php
@@ -2,8 +2,4 @@
 
 define('APP_PATH', __DIR__ . '/../application/');
 // 加载框架引导文件
-if (strpos($_SERVER['SERVER_ADDR'], '127.0.0.1') !== FALSE) {
-    require __DIR__ .'/../thinkphp/start.php';
-} else {
-    require '../thinkphp/start.php';
-}
+require __DIR__ .'/../thinkphp/start.php';
